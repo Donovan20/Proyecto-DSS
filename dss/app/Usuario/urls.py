8478d76bab6494cfd4c9758a8e1273d0fa81f83f
Edgar Gómez Zárate <egomez05@alumnos.uaq.mx>
2019-05-26 12:24:33 -0500

    Mejorado

--- dss/app/Usuario/urls.py
+++ dss/app/Usuario/urls.py
@@ -4,10 +4,11 @@ from django.urls import path
 # Modelos
 from app.Usuario.views import login_sistema
 from app.Usuario.views import logout_view
+from app.Usuario.views import variables_view
 
 app_name='usuario'
 urlpatterns = [
     path('', login_sistema, name='login'),
-    path('logout_view',logout_view,name="logout"),    
-
+    path('logout_view/',logout_view,name="logout"),    
+    path('<username>/variables/',variables_view,name="variables"),    
 ]
