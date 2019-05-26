82099fdf910bfe8d1247f263ebbce12f6cc29852
Edgar Gómez Zárate <egomez05@alumnos.uaq.mx>
2019-05-26 10:59:18 -0500

    Decimal places

--- dss/app/Usuario/models.py
+++ dss/app/Usuario/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class PIB(models.Model):
     periodo = models.IntegerField(max_length=5)
-    frecuencia = models.DecimalField(max_digits=10)
+    frecuencia = models.DecimalField(max_digits=10, decimal_places=4)
 
 class Dolar(models.Model):
     periodo = models.IntegerField(max_length=5)
-    frecuencia = models.DecimalField(max_digits=10)
+    frecuencia = models.DecimalField(max_digits=10, decimal_places=16)
 
 
