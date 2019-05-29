2fcaa591711b9eef520539500e59d5880665a913
Edgar Gómez Zárate <egomez05@alumnos.uaq.mx>
2019-05-29 09:04:15 -0500

    Arreglitos por ahí

--- dss/app/Usuario/views.py
+++ dss/app/Usuario/views.py
@@ -655,8 +655,8 @@ def configuraciones_pib(request,username):
                             epmda.insert(103,0)
                             epsel.insert(103,0)
                             epmd.insert(103,0)
-                            # epms[102] = 0
-                            # eps[102] = 0
+                            epms[102] = 0
+                            eps[102] = 0
                             eptmac.insert(103,0)
                             ptmac.insert(0,0)
                             ptmac.insert(0,0)
